beab9659bbbcca3a12c5398445b6ee3d292a51c1
merhinasser33-collab <merhi.nasser.33@gmail.com>
2025-12-26 13:35:42 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+// firebase.js
+export const firebaseConfig = {
+  apiKey: "PUT_YOUR_API_KEY",
+  authDomain: "PUT_YOUR_AUTH_DOMAIN",
+  projectId: "PUT_YOUR_PROJECT_ID",
+  appId: "PUT_YOUR_APP_ID",
+};
